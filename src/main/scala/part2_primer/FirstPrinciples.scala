5f1eb2d3efde86cacb7e7123d6ae6ec25d31b43d
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 18:41:19 -0300

    very first akka streams, seems even easier than java8 streams api

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -2,7 +2,7 @@ package part2_primer
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object FirstPrinciples extends App {
 
@@ -21,4 +21,31 @@ object FirstPrinciples extends App {
 
 
   graph.run()
+
+
+  //flows transforms elements
+
+  val flow = Flow[Int].map(_ + 1)
+  val sourceWithFlow = source.via(flow)
+
+  /**
+    * Exercice create a stream of person , take the first 2 names longer than 5 char.
+    * print results in the end
+    *
+    */
+
+    case class Person(name: String)
+
+  val listPerson = List(Person("Ann"), Person("Tonny"), Person("August"), Person("Troy"),Person("Amanda"))
+  val sourcePerson = Source(listPerson)
+  val longNameFlow = Flow[String].filter(s => s.length > 5)
+  val firtTwo = Flow[String].take(2)
+
+  sourcePerson.map(p => p.name)
+    .via(longNameFlow)
+    .via(firtTwo)
+    .to(Sink.foreach(println)).run()
+
+  sourcePerson.filter(_.name.length > 5).take(2).map(_.name).runForeach(println)
+
 }
