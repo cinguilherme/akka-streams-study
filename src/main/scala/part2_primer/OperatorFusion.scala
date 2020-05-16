88e1d7a3f7f03082e43fb12b2d783f1c1dcd6295
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 20:25:44 -0300

    understanding ordering with fusion and with async

--- src/main/scala/part2_primer/OperatorFusion.scala
+++ src/main/scala/part2_primer/OperatorFusion.scala
@@ -10,7 +10,7 @@ object OperatorFusion extends App {
   implicit val system  = ActorSystem("operatorFusion")
   implicit val materializer = ActorMaterializer()
 
-  val simpleSource = Source(1 to 1000)
+  val simpleSource = Source(1 to 10)
 
   val simpleFlow = Flow[Int].map(_ + 1)
   val simpleFlow2 = Flow[Int].map(_ * 10)
@@ -41,5 +41,20 @@ object OperatorFusion extends App {
     .via(complexFlow).async // another actor
     .via(complexFlow2).async // another actor
     .to(Sink.foreach[Int](i => println(s"fusion broken pipeline $i"))).run() // this should be twice as fast give or take
+
+  // ordering guarantees
+  Source(1 to 3)
+    .map(x => {println(s"A -> $x"); x})
+    .map(x => {println(s"B -> $x"); x})
+    .map(x => {println(s"C -> $x"); x})
+    .runWith(Sink.ignore)
+  // WITH fusion is 100% deterministic complete results. the entire stream is processed for each element
+  // WITH ASYNC the is relative order, A,B,C is ordered for each relative element.
+  // But not for the complete pipe
+  Source(1 to 3)
+    .map(x => {println(s"A -> $x"); x}).async
+    .map(x => {println(s"B -> $x"); x}).async
+    .map(x => {println(s"C -> $x"); x}).async
+    .runWith(Sink.ignore)
 
 }
