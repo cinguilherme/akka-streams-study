92d2a82fc360c4f446bcd10359f65d77f4cbe178
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 17:26:28 -0300

    akka actors recap completed, actors with tell and ask patterns

--- src/main/scala/part1_recap/AkkaRecap.scala
+++ src/main/scala/part1_recap/AkkaRecap.scala
@@ -1,17 +1,20 @@
 package part1_recap
 
 import akka.actor.{Actor, ActorSystem, Props, Stash}
+import akka.pattern.AskableActorRef
+import akka.util.Timeout
 
 object AkkaRecap extends App {
 
-  class SimpleActor extends Actor with Stash{
+  class SimpleActor extends Actor with Stash {
     override def receive: Receive = {
       case "createChild" =>
         val child = context.actorOf(Props[SimpleActor], "myChild")
         child ! "say hello child"
       case "stashThis" => stash()
-      case message => println(s"i received $message")
       case "change" => context.become(anotherHandler)
+      case "question" => sender() ! "hummm, this is a response from another actor.."
+      case message => println(s"i received $message")
     }
 
     def anotherHandler: Receive = {
@@ -24,8 +27,19 @@ object AkkaRecap extends App {
   val system = ActorSystem("AkkaRecap")
 
   val actor = system.actorOf(Props[SimpleActor], "simpleActor")
+  val anotherActor = system.actorOf(Props[SimpleActor], "anotherSimpleActor")
 
   actor ! "Hiiiii"
 
+  import system.dispatcher
+  import scala.concurrent.duration._
+  import akka.pattern.ask
+  import akka.pattern._
+
+  implicit val timeout: Timeout = Timeout(3 seconds)
+
+  val future = actor ? "question"
+
+  future.mapTo[String].pipeTo(anotherActor)
 
 }
