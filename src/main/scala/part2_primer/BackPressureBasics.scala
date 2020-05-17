524b77d0c8a7b24deed161263b32731402743597
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 21:24:50 -0300

    back pressure rocks really hard

--- src/main/scala/part2_primer/BackPressureBasics.scala
+++ src/main/scala/part2_primer/BackPressureBasics.scala
@@ -1,7 +1,7 @@
 package part2_primer
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
 object BackPressureBasics extends App {
@@ -20,15 +20,35 @@ object BackPressureBasics extends App {
 
   //fastSource.async.to(slowSink).run() // backpressure is in place, we have a new actor for the sink.. pero no mucho
 
+  import scala.concurrent.duration._
+
   val simpleFlow = Flow[Int].map {x => println(s"incoming $x"); x+1}
+
+  val bffFlow = Flow[Int].buffer(10, OverflowStrategy.backpressure)
+    .map(x => {println(s"intake on buffered $x"); x})
+
+
+  val simpleFlow2 = Flow[Int].throttle(10, 1 second)
+    .map(x => { println(s"intake on the simpleFlow 2 $x"); x + 1})
 
   // this here is awesome. back pressure at its finest
   fastSource.async
-    .via(simpleFlow).async
-    .to(slowSink)
-    .run()
+  .via(simpleFlow).async
+  .to(slowSink)
+  //    .run()
 
   /**
     * How this components reacts to back pressure?
     */
+  fastSource.throttle(2, 1 second).to(Sink.foreach(println))
+//    .run() //manual backpressure using throtle
+
+  val retrySink = Sink.queue()
+
+  fastSource
+    .throttle(100, 1 second)
+    .via(bffFlow).async
+    .via(simpleFlow2).async
+    .to(slowSink)
+    .run()
 }
