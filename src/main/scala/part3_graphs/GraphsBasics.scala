f6cd2c2aca814dade9efdd96b796d0ef8319e308
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-17 11:29:27 -0300

    ComplexGraphs initial run

--- src/main/scala/part3_graphs/GraphsBasics.scala
+++ src/main/scala/part3_graphs/GraphsBasics.scala
@@ -1,7 +1,9 @@
 package part3_graphs
 
+import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
 
 object GraphsBasics extends App {
 
@@ -9,5 +11,30 @@ object GraphsBasics extends App {
   implicit val materializer = ActorMaterializer()
 
 
+  val input = Source(1 to 1000)
+
+  val output = Sink.foreach[(Int, Int)](println)
+
+  val incrementer = Flow[Int].map(_ + 1)
+  val multiplier = Flow[Int].map(_ * 10)
+
+  val complexGraph = RunnableGraph
+    .fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+    import GraphDSL.Implicits._
+
+      // create necessary components for the Graph
+      val broadCast = builder.add(Broadcast[Int](2))
+      val zip = builder.add(Zip[Int, Int])
+
+      input ~> broadCast
+      broadCast.out(0) ~> incrementer ~> zip.in0
+      broadCast.out(1) ~> multiplier ~> zip.in1
+
+      zip.out ~> output
+
+      ClosedShape
+  })
+
+  complexGraph.run()
 
 }
