dd6d1934f9732c993b57e03b593f4156fd5e8f3a
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-17 14:28:54 -0300

    susissius transaction flow as a ClosedShape

--- src/main/scala/part3_graphs/MoreOpenGraphs.scala
+++ src/main/scala/part3_graphs/MoreOpenGraphs.scala
@@ -1,8 +1,8 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape, UniformFanInShape}
-import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source, ZipWith}
+import akka.stream.{ActorMaterializer, ClosedShape, FanOutShape, UniformFanInShape}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, ZipWith}
 
 object MoreOpenGraphs extends App {
 
@@ -51,6 +51,42 @@ object MoreOpenGraphs extends App {
 
   /**
     * Suspicious transaction detection component
+    * - NON uniform fanOut Shape
+    * stream component for Trx type
+    * - output 1 -> unmodified Trx pass
+    * - output 2 -> trx ID as String to be further analized
     */
+
+  case class Transation(id: String, ammount: BigDecimal)
+
+  val list = List(
+    Transation("1", 10000),
+    Transation("2", 500),
+    Transation("3", 20000))
+
+  val trxSource = Source(list)
+
+  val regularSink = Sink.foreach[Transation](t => println(s"regular sink, transacion comming through $t"))
+  val suspiciousSink = Sink.foreach[String](id => println(s"this transaction is suspissious, ID $id"))
+
+  val f = RunnableGraph.fromGraph( GraphDSL.create() {
+    implicit builder =>
+
+
+      val broadcast = builder.add(Broadcast[Transation](2))
+
+      trxSource ~> broadcast
+
+      broadcast.out(0) ~> regularSink
+
+      val filterFlow = builder.add(Flow[Transation].filter(t => t.ammount > 9999).map(t => t.id))
+
+      broadcast.out(1) ~> filterFlow ~> suspiciousSink
+
+      ClosedShape
+  })
+  f.run()
+
+
 
 }
